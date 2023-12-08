1da4ffe24284118e879785e31735667cf594bda9
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2023-12-08 18:58:01 +0100

    add initial requirements

--- src/app/page-click-counter/page-click-counter.component.ts
+++ src/app/page-click-counter/page-click-counter.component.ts
@@ -8,4 +8,18 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './page-click-counter.component.html',
   styleUrl: './page-click-counter.component.scss',
 })
-export class PageClickCounterComponent {}
+export class PageClickCounterComponent {
+
+  /** 1. Create an Observable of page clicks: clicks$ */ 
+  /** 2. Use the Observable above to create 2 others:
+   *     a) leftSideClicks$ - clicks on the left side of the page (document);
+   *     b) rightSideClicks$ - clicks on the right side of the page (document);
+   * 3. Map events in leftSideClicks$ to a string 'left';
+   * 4. Map events in rightSideClicks$ to a string 'right';
+   * 5. In template instead of hard-coded 'UNKNOWN', display page side that was clicked the last time;
+   * 6. Configure the initial value for the observable as a string 'UNKNOWN'
+   * 7. Implement click counters for both sides and display them in component template
+   */
+  
+    ngOnInit() {}
+}
